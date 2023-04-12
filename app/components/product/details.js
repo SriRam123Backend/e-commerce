dd59c6d4ff17d03ec4c93fa9fe4e505fc79cf9b0
SriRam123Backend <sriramtsi000@gmail.com>
2023-04-12 15:46:03 +0530

    commit

--- app/components/product/details.js
+++ app/components/product/details.js
@@ -1,3 +1,13 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
-export default class ProductDetailsComponent extends Component {}
+export default class ItemDetailsComponent extends Component {
+  @service cart;
+
+  @action
+  addToCart() {
+    const { product, color } = this.args;
+    this.cart.add(product, color);
+  }
+}
